d1d2b25ba43cbd6988f6b178f3da1d8742a4b65f
Syed Askari <57672409+askai1122@users.noreply.github.com>
2026-05-23 03:26:37 +0500

    fix vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      preset: "vercel",
-    },
-  },
+  base: "/",
+  plugins: []
 });
